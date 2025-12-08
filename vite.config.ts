7624fe8365821b73f5844e07b9844a04ede0c2cb
AdarshKumarPriyadarshi <Adarsh123150@gmail.com>
2025-12-08 15:44:49 +0530

    Refactor base path comment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: "/Adarsh-Portfolio/",   // <<< important for GitHub Pages
+  base: "/Adarsh-Portfolio/",   // <-- important
   server: {
     host: "::",
     port: 8080,
